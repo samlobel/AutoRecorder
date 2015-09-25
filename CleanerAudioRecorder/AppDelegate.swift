4fcc6e21dd65bf364fbf5017dc12bba3403f3b6c
Sam Lobel <samlobel@yahoo.com>
2015-09-25 13:05:34 -0400

    Initial Commit

---
+++ CleanerAudioRecorder/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CleanerAudioRecorder
+//
+//  Created by Sam Lobel on 9/25/15.
+//  Copyright © 2015 Sam Lobel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
